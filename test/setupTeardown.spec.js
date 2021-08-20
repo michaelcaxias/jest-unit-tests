f989edccdaf500bb646df131cbde99cd929f7b54
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-08-20 13:55:00 -0300

    Req 4: Completo

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -20,7 +20,7 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('4 - Quem sobreviveu?', () => {
-  // Adicione seu código aqui
+  beforeEach(() => adventure.randomAttack());
 
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
