171993363359dbc5740a08698694166115eac430
michaelcaxias <michaelcaxias.dev@gmail.com>
2021-08-20 13:28:41 -0300

    Req 2: Completo

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -15,7 +15,14 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('3 - Verifica as funções e os mocks', () => {
-  // Crie suas mock functions aqui
+  mockFunctions.add = jest.fn().mockImplementation((a, b) => a + b);
+  mockFunctions.subtract = jest.fn().mockImplementation((a, b) => a - b);
+  mockFunctions.multiply = jest.fn().mockImplementation((a, b) => a * b);
+  mockFunctions.divide = jest.fn().mockImplementation((a, b) => a / b);
+  mockFunctions.power = jest.fn().mockImplementation((a, b) => a ** b);
+  // https://www.ti-enxame.com/pt/javascript/qual-e-funcao-fatorial-mais-rapida-em-javascript/970711875/
+  const factorialNumber = (n) => ((n > 0) ? n * factorialNumber(n - 1) : 1);
+  mockFunctions.factorial = jest.fn().mockImplementation(factorialNumber);
 
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
